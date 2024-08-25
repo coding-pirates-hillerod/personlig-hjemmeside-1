c8c4983a313d1f3300ce3b23b57466e5e77ad4a3
Daniel Massoudyan <47738720+massoudyan1@users.noreply.github.com>
2024-08-25 18:12:26 +0200

    Make my own sketch

--- sketch.js
+++ sketch.js
@@ -1,392 +1,114 @@
-/*
-Frozen brush
+// Daniel's Particle brush
 
-Makes use of a delaunay algorithm to create crystal-like shapes.
-I did NOT develop delaunay.js, and not sure who the author really is to give proper credit.
-
-Controls:
-	- Drag the mouse.
-    - Press any key to toggle between fill and stroke.
-
-Inspired by:
-	Makio135's sketch www.openprocessing.org/sketch/385808
-
-Author:
-  Jason Labbe
-
-Site:
-  jasonlabbe3d.com
-*/
-
-var allParticles = [];
-var maxLevel = 5;
-var useFill = false;
-
-var data = [];
-
-
-// Moves to a random direction and comes to a stop.
-// Spawns other particles within its lifetime.
-function Particle(x, y, level) {
-  this.level = level;
-  this.life = 0;
-  
-  this.pos = new p5.Vector(x, y);
-  this.vel = p5.Vector.random2D();
-  this.vel.mult(map(this.level, 0, maxLevel, 5, 2));
-  
-  this.move = function() {
-    this.life++;
-    
-    // Add friction.
-    this.vel.mult(0.9);
-    
-    this.pos.add(this.vel);
-    
-    // Spawn a new particle if conditions are met.
-    if (this.life % 10 == 0) {
-      if (this.level > 0) {
-        this.level -= 1;
-        var newParticle = new Particle(this.pos.x, this.pos.y, this.level-1);
-        allParticles.push(newParticle);
-      }
-    }
-  }
-}
-
+let particles = []; // Array to store all particles
+let speed = 1.9;
+let connectRadius = 100;
+let decelerationFactor = 0.98;
+let particleLifespan = 40;
+let prevMouseX, prevMouseY; // Variables to store the previous mouse position
 
 function setup() {
-  let canvas = createCanvas(windowWidth, windowHeight); 
-  canvas.class("lemon");
+  createCanvas(windowWidth, windowHeight);
+  strokeWeight(0.1);
 
-  colorMode(HSB, 360);
-  
-  textAlign(CENTER);
-  
-  background(0);
-} 
-
+  // Initialize previous mouse positions
+  prevMouseX = mouseX;
+  prevMouseY = mouseY;
+}
 
 function draw() {
-  // Create fade effect.
-  noStroke();
-  fill(0, 30);
-  rect(0, 0, width, height);
-  
-  // Move and spawn particles.
-  // Remove any that is below the velocity threshold.
-  for (var i = allParticles.length-1; i > -1; i--) {
-    allParticles[i].move();
-    
-    if (allParticles[i].vel.mag() < 0.01) {
-      allParticles.splice(i, 1);
-    }
-  }
-  
-  if (allParticles.length > 0) {
-    // Run script to get points to create triangles with.
-    data = Delaunay.triangulate(allParticles.map(function(pt) {
-      return [pt.pos.x, pt.pos.y];
-    }));
-  	
-    strokeWeight(0.1);
-    
-    // Display triangles individually.
-    for (var i = 0; i < data.length; i += 3) {
-      // Collect particles that make this triangle.
-      var p1 = allParticles[data[i]];
-      var p2 = allParticles[data[i+1]];
-      var p3 = allParticles[data[i+2]];
-      
-      // Don't draw triangle if its area is too big.
-      var distThresh = 75;
-      
-      if (dist(p1.pos.x, p1.pos.y, p2.pos.x, p2.pos.y) > distThresh) {
-        continue;
-      }
-      
-      if (dist(p2.pos.x, p2.pos.y, p3.pos.x, p3.pos.y) > distThresh) {
-        continue;
-      }
-      
-      if (dist(p1.pos.x, p1.pos.y, p3.pos.x, p3.pos.y) > distThresh) {
-        continue;
-      }
-      
-      // Base its hue by the particle's life.
-      if (useFill) {
-        noStroke();
-        fill(165+p1.life*1.5, 360, 360);
-      } else {
-        noFill();
-        stroke(165+p1.life*1.5, 360, 360);
-      }
-      
-      triangle(p1.pos.x, p1.pos.y, 
-               p2.pos.x, p2.pos.y, 
-               p3.pos.x, p3.pos.y);
-    }
-  }
-  
-  noStroke();
-}
+  background(0);
 
-
-function mouseDragged() {
-  allParticles.push(new Particle(mouseX, mouseY, maxLevel));
-}
-
-
-function keyPressed() {
-  useFill = ! useFill;
-}
-
-function windowResized() {
-	if (fullscreen()) resizeCanvas(displayWidth, displayHeight);
-	else resizeCanvas(windowWidth, windowHeight);
-}
-
-/*
-Orginally from https://cdn.rawgit.com/ironwallaby/delaunay/master/delaunay.js
-Tweaked it so instead of raising an error it would return an empty list.
-*/
-
-var Delaunay;
-
-(function() {
-  "use strict";
-
-  var EPSILON = 1.0 / 1048576.0;
-
-  function supertriangle(vertices) {
-    var xmin = Number.POSITIVE_INFINITY,
-        ymin = Number.POSITIVE_INFINITY,
-        xmax = Number.NEGATIVE_INFINITY,
-        ymax = Number.NEGATIVE_INFINITY,
-        i, dx, dy, dmax, xmid, ymid;
-
-    for(i = vertices.length; i--; ) {
-      if(vertices[i][0] < xmin) xmin = vertices[i][0];
-      if(vertices[i][0] > xmax) xmax = vertices[i][0];
-      if(vertices[i][1] < ymin) ymin = vertices[i][1];
-      if(vertices[i][1] > ymax) ymax = vertices[i][1];
-    }
-
-    dx = xmax - xmin;
-    dy = ymax - ymin;
-    dmax = Math.max(dx, dy);
-    xmid = xmin + dx * 0.5;
-    ymid = ymin + dy * 0.5;
-
-    return [
-      [xmid - 20 * dmax, ymid -      dmax],
-      [xmid            , ymid + 20 * dmax],
-      [xmid + 20 * dmax, ymid -      dmax]
-    ];
+  // Check if the mouse has moved
+  if (mouseX !== prevMouseX || mouseY !== prevMouseY) {
+    // If the mouse has moved, create a new particle
+    let p = new Particle(mouseX, mouseY);
+    particles.push(p);
   }
 
-  function circumcircle(vertices, i, j, k) {
-    var x1 = vertices[i][0],
-        y1 = vertices[i][1],
-        x2 = vertices[j][0],
-        y2 = vertices[j][1],
-        x3 = vertices[k][0],
-        y3 = vertices[k][1],
-        fabsy1y2 = Math.abs(y1 - y2),
-        fabsy2y3 = Math.abs(y2 - y3),
-        xc, yc, m1, m2, mx1, mx2, my1, my2, dx, dy;
+  // Update previous mouse positions
+  prevMouseX = mouseX;
+  prevMouseY = mouseY;
 
-    /* Check for coincident points */
-    if(fabsy1y2 < EPSILON && fabsy2y3 < EPSILON)
-      return;
-      //throw new Error("Eek! Coincident points!");
+  // Loop through all particles in reverse order to update and display them
+  for (let i = particles.length - 1; i >= 0; i--) {
+    particles[i].update();
+    particles[i].display();
 
-    if(fabsy1y2 < EPSILON) {
-      m2  = -((x3 - x2) / (y3 - y2));
-      mx2 = (x2 + x3) / 2.0;
-      my2 = (y2 + y3) / 2.0;
-      xc  = (x2 + x1) / 2.0;
-      yc  = m2 * (xc - mx2) + my2;
+    // Check if the particle's lifespan is over
+    if (particles[i].lifespan <= 0) {
+      particles.splice(i, 1); // Remove the particle from the array
     }
-
-    else if(fabsy2y3 < EPSILON) {
-      m1  = -((x2 - x1) / (y2 - y1));
-      mx1 = (x1 + x2) / 2.0;
-      my1 = (y1 + y2) / 2.0;
-      xc  = (x3 + x2) / 2.0;
-      yc  = m1 * (xc - mx1) + my1;
-    }
-
-    else {
-      m1  = -((x2 - x1) / (y2 - y1));
-      m2  = -((x3 - x2) / (y3 - y2));
-      mx1 = (x1 + x2) / 2.0;
-      mx2 = (x2 + x3) / 2.0;
-      my1 = (y1 + y2) / 2.0;
-      my2 = (y2 + y3) / 2.0;
-      xc  = (m1 * mx1 - m2 * mx2 + my2 - my1) / (m1 - m2);
-      yc  = (fabsy1y2 > fabsy2y3) ?
-        m1 * (xc - mx1) + my1 :
-        m2 * (xc - mx2) + my2;
-    }
-
-    dx = x2 - xc;
-    dy = y2 - yc;
-    return {i: i, j: j, k: k, x: xc, y: yc, r: dx * dx + dy * dy};
   }
 
-  function dedup(edges) {
-    var i, j, a, b, m, n;
+  // Check for particles within a radius of 50 and draw lines if 3 or more are close
+  checkForCloseParticles();
+}
 
-    for(j = edges.length; j; ) {
-      b = edges[--j];
-      a = edges[--j];
+// Function to check for close particles and draw lines
+function checkForCloseParticles() {
+  let closeGroups = []; // Array to store groups of close particles
 
-      for(i = j; i; ) {
-        n = edges[--i];
-        m = edges[--i];
+  // Loop through all particles to find groups of close particles
+  for (let i = 0; i < particles.length; i++) {
+    let closeParticles = [particles[i]]; // Start a new group with the current particle
 
-        if((a === m && b === n) || (a === n && b === m)) {
-          edges.splice(j, 2);
-          edges.splice(i, 2);
-          break;
+    // Check distances to all other particles
+    for (let j = 0; j < particles.length; j++) {
+      if (i != j) {
+        let d = dist(particles[i].x, particles[i].y, particles[j].x, particles[j].y);
+        if (d <= connectRadius) {
+          closeParticles.push(particles[j]); // Add particle to the group if it's within radius
         }
       }
     }
+
+    // If 3 or more particles are close, add this group to closeGroups
+    if (closeParticles.length >= 3) {
+      closeGroups.push(closeParticles);
+    }
   }
 
-  Delaunay = {
-    triangulate: function(vertices, key) {
-      var n = vertices.length,
-          i, j, indices, st, open, closed, edges, dx, dy, a, b, c;
-
-      /* Bail if there aren't enough vertices to form any triangles. */
-      if(n < 3)
-        return [];
-
-      /* Slice out the actual vertices from the passed objects. (Duplicate the
-       * array even if we don't, though, since we need to make a supertriangle
-       * later on!) */
-      vertices = vertices.slice(0);
-
-      if(key)
-        for(i = n; i--; )
-          vertices[i] = vertices[i][key];
-
-      /* Make an array of indices into the vertex array, sorted by the
-       * vertices' x-position. */
-      indices = new Array(n);
-
-      for(i = n; i--; )
-        indices[i] = i;
-
-      indices.sort(function(i, j) {
-        return vertices[j][0] - vertices[i][0];
-      });
-
-      /* Next, find the vertices of the supertriangle (which contains all other
-       * triangles), and append them onto the end of a (copy of) the vertex
-       * array. */
-      st = supertriangle(vertices);
-      vertices.push(st[0], st[1], st[2]);
-      
-      /* Initialize the open list (containing the supertriangle and nothing
-       * else) and the closed list (which is empty since we havn't processed
-       * any triangles yet). */
-      var circCircle = circumcircle(vertices, n + 0, n + 1, n + 2);
-      if (circCircle == undefined)
-        return [];
-      
-      open   = [circumcircle(vertices, n + 0, n + 1, n + 2)];
-      closed = [];
-      edges  = [];
-      
-      /* Incrementally add each vertex to the mesh. */
-      for(i = indices.length; i--; edges.length = 0) {
-        c = indices[i];
-
-        /* For each open triangle, check to see if the current point is
-         * inside it's circumcircle. If it is, remove the triangle and add
-         * it's edges to an edge list. */
-        for(j = open.length; j--; ) {
-          /* If this point is to the right of this triangle's circumcircle,
-           * then this triangle should never get checked again. Remove it
-           * from the open list, add it to the closed list, and skip. */
-          dx = vertices[c][0] - open[j].x;
-          if(dx > 0.0 && dx * dx > open[j].r) {
-            closed.push(open[j]);
-            open.splice(j, 1);
-            continue;
-          }
-
-          /* If we're outside the circumcircle, skip this triangle. */
-          dy = vertices[c][1] - open[j].y;
-          if(dx * dx + dy * dy - open[j].r > EPSILON)
-            continue;
-
-          /* Remove the triangle and add it's edges to the edge list. */
-          edges.push(
-            open[j].i, open[j].j,
-            open[j].j, open[j].k,
-            open[j].k, open[j].i
-          );
-          open.splice(j, 1);
-        }
-
-        /* Remove any doubled edges. */
-        dedup(edges);
-
-        /* Add a new triangle for each edge. */
-        for(j = edges.length; j; ) {
-          b = edges[--j];
-          a = edges[--j];
-          open.push(circumcircle(vertices, a, b, c));
-        }
+  // Draw lines between particles in each group
+  for (let group of closeGroups) {
+    for (let i = 0; i < group.length; i++) {
+      for (let j = i + 1; j < group.length; j++) {
+        stroke(100, 100, 255, 60);
+        line(group[i].x, group[i].y, group[j].x, group[j].y);
       }
-
-      /* Copy any remaining open triangles to the closed list, and then
-       * remove any triangles that share a vertex with the supertriangle,
-       * building a list of triplets that represent triangles. */
-      for(i = open.length; i--; )
-        closed.push(open[i]);
-      open.length = 0;
-
-      for(i = closed.length; i--; )
-        if(closed[i].i < n && closed[i].j < n && closed[i].k < n)
-          open.push(closed[i].i, closed[i].j, closed[i].k);
-
-      /* Yay, we're done! */
-      return open;
-    },
-    contains: function(tri, p) {
-      /* Bounding box test first, for quick rejections. */
-      if((p[0] < tri[0][0] && p[0] < tri[1][0] && p[0] < tri[2][0]) ||
-         (p[0] > tri[0][0] && p[0] > tri[1][0] && p[0] > tri[2][0]) ||
-         (p[1] < tri[0][1] && p[1] < tri[1][1] && p[1] < tri[2][1]) ||
-         (p[1] > tri[0][1] && p[1] > tri[1][1] && p[1] > tri[2][1]))
-        return null;
-
-      var a = tri[1][0] - tri[0][0],
-          b = tri[2][0] - tri[0][0],
-          c = tri[1][1] - tri[0][1],
-          d = tri[2][1] - tri[0][1],
-          i = a * d - b * c;
-
-      /* Degenerate tri. */
-      if(i === 0.0)
-        return null;
-
-      var u = (d * (p[0] - tri[0][0]) - b * (p[1] - tri[0][1])) / i,
-          v = (a * (p[1] - tri[0][1]) - c * (p[0] - tri[0][0])) / i;
-
-      /* If we're outside the tri, fail. */
-      if(u < 0.0 || v < 0.0 || (u + v) > 1.0)
-        return null;
-
-      return [u, v];
     }
-  };
+  }
+}
 
-  if(typeof module !== "undefined")
-    module.exports = Delaunay;
-})();
+// Particle class
+class Particle {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.lifespan = particleLifespan; // Lifespan set to 40 frames
+
+    // Generate a random speed and direction
+    this.vx = random(-speed, speed); // Velocity in the x direction
+    this.vy = random(-speed, speed); // Velocity in the y direction
+
+    this.deceleration = decelerationFactor; // Deceleration factor
+  }
+
+  // Method to update position and decrease lifespan
+  update() {
+    this.x += this.vx; // Update x position
+    this.y += this.vy; // Update y position
+
+    // Apply deceleration to velocities
+    this.vx *= this.deceleration;
+    this.vy *= this.deceleration;
+
+    this.lifespan -= 1; // Decrease lifespan
+  }
+
+  // Method to display the particle
+  display() {
+    stroke(0);
+    point(this.x, this.y);
+  }
+}
